895a456ce2f8485f367e18aadb85c8e2e99d1067
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-25 17:18:05 +0300

    switch case

--- utilities.dart
+++ utilities.dart
@@ -13,8 +13,33 @@
 // }
   
 
-  void main() {
-     for (int i = 1; i <= 10 ; i++) {
-        print(i);
-     }
-  }
+//   void main() {
+//      for (int i = 1; i <= 10 ; i++) {
+//         print(i);
+//      }
+//   }
+
+void main() {
+    String vowels = ["a", "e", "i","o", "u"]
+    switch (vowels) {
+        case 1:
+        print("The first vowel is:" + vowels[0]);
+        break;
+        case 2:
+        print("The second vowel is:" + vowels[1]);
+        break;
+        case 3:
+        print("The third vowel is:" + vowels[2]);
+        break;
+        case 4:
+        print("The fourth vowel is:" + vowels[3]);
+        break;
+        case 5:
+        print("The fifth vowel is:" + vowels[4]);
+        break;
+        default:
+        print("No vowels");
+        break;
+
+    }
+}
