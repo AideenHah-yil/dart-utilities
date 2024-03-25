5fd29354293e21af64b3c5666f4c95bab8c616ff
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-25 17:52:30 +0300

    final task

--- utilities.dart
+++ utilities.dart
@@ -1,85 +1,63 @@
 import 'dart:io';
 
-// int add(int a, int b){
-//  int sum = a + b;
-//    return sum;
-
-// }
-
-// void main() {
-//     int a = 10;
-//     int b = 30;
-
-//     int sum = add(a, b);
-//     print(sum);
-// }
-  
-
-//   void main() {
-//      for (int i = 1; i <= 10 ; i++) {
-//         print(i);
-//      }
-//   }
-
-// void main() {
-//     List<String> vowels = ["a", "e", "i","o", "u"];
-//     String input = "a";
-//     switch (input) {
-//         case "a":
-//         print("'$input' is a vowel");
-//         break;
-//         case "e":
-//         print("$input' is a vowel");
-//         break;
-//         case "i":
-//         print("$input' is a vowel");
-//         break;
-//         case "o":
-//         print("$input' is a vowel");
-//         break;
-//         case "u":
-//         print("$input' is a vowel");
-//         break;
-//         default:
-//         print("$input' is not a vowel");
-//         break;
-//     }
-// }
-
-// void main() {
-//     var num = 11;
-//     if (num % 2 == 0) {
-//         print("'$num' is even");
-//     }else if (num % 2 == 1) {
-//         print("'$num' is odd");
-//     }    
-// }
-
-
-
-void main() {
-    print("Enter your numbers separated by spaces:");
-
-    String input = stdin.readLineSync()!;
-
-    List<String> numbersAsString = input.split(" ");
-
-    List<int> numbers = numbersAsString.map(int.parse).toList();
-
-    int largestNumber = numbers.reduce((value, element) => value > element ? value : element);
-
-    print("Largest number is $largestNumber");
+int add(int a, int b) {
+  int sum = a + b;
+  return sum;
 }
 
-
 void main() {
-try {
-    int result = 20 ~/ 0;
+  // Task 1: Function to add two numbers
+  int a = 10;
+  int b = 30;
+  int sum = add(a, b);
+  print("Task 1: Sum of $a and $b is $sum");
 
+  // Task 2: Print numbers from 1 to 10 using a for loop
+  print("\nTask 2:");
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+
+  // Task 3: Check if a string input is a vowel
+  print("\nTask 3:");
+  List<String> vowels = ["a", "e", "i", "o", "u"];
+  String input = "a";
+  switch (input) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+      print("'$input' is a vowel");
+      break;
+    default:
+      print("'$input' is not a vowel");
+      break;
+  }
+
+  // Task 4: Check if a number is even or odd
+  print("\nTask 4:");
+  var num = 11;
+  if (num % 2 == 0) {
+    print("'$num' is even");
+  } else {
+    print("'$num' is odd");
+  }
+
+  // Task 5: Find the largest number in a list
+  print("\nTask 5:");
+  print("Enter your numbers separated by spaces:");
+  String inputNumbers = stdin.readLineSync()!;
+  List<int> numbers = inputNumbers.split(" ").map(int.parse).toList();
+  int largestNumber = numbers.reduce((value, element) => value > element ? value : element);
+  print("Largest number is $largestNumber");
+
+  // Task 6: Handle exception using try-catch block
+  print("\nTask 6:");
+  try {
+    int result = 10 ~/ 0;
     print(result);
-} catch (e) { 
-    print("An error occurred");
-}
-
-
+  } catch (e) {
+    print("An error occurred: $e");
+  }
 }
